c361853d10576ce02ee07a22e087bc32ad0193d3
Biyu-GitHub <790125037@qq.com>
2019-08-11 11:33:02 +0800

    project init

---
+++ src/main/java/com/biyu2019/Miaosha2Application.java
@@ -0,0 +1,13 @@
+package com.biyu2019;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Miaosha2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Miaosha2Application.class, args);
+    }
+
+}
